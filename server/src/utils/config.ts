bdd1225d50ae024c3fafc63858de5d6f4df3261f
Ahmed Kasu <ankassu@gmail.com>
2023-10-20 16:13:38 +0300

    added DATABASE_URL env variable

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -3,6 +3,7 @@ import { load } from 'ts-dotenv';
 type Env = {
   PORT?: number;
   NODE_ENV?: 'development' | 'production' | 'test';
+  DATABASE_URL: string;
 };
 
 const env: Env = load({
@@ -12,9 +13,11 @@ const env: Env = load({
     optional: true,
     default: 'development',
   },
+  DATABASE_URL: String,
 }) as Env;
 
 const PORT = env.PORT;
 const NODE_ENV = env.NODE_ENV;
+const DATABASE_URL = env.DATABASE_URL;
 
-export { NODE_ENV, PORT };
+export { NODE_ENV, PORT, DATABASE_URL };
