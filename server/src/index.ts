280408bc1645a2792cd390aa612af19a542c15b1
Ahmed Kasu <ankassu@gmail.com>
2023-10-20 14:39:09 +0300

    replaced hardcoded port with an env variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+
+import { PORT } from './utils/config';
+
 const app = express();
 
 app.use(express.json());
-
-const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
