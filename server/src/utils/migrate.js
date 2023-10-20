be8ae8fc1e89cfebdbdef3e36876155049f0554b
Ahmed Kasu <ankassu@gmail.com>
2023-10-20 18:44:11 +0300

    added a js file to run migrations without the need to compile

---
+++ server/src/utils/migrate.js
@@ -0,0 +1,10 @@
+require('ts-node/register');
+
+require('./umzug')
+  .migrator.runAsCLI()
+  .then(() => {
+    console.log('Migration complete');
+  })
+  .catch((error) => {
+    console.error('Error during migration:', error);
+  });
