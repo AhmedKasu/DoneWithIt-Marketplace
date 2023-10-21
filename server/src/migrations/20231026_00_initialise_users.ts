f4dd8421476b549974efed707903eab15b2b83b5
Ahmed Kasu <ankassu@gmail.com>
2023-10-21 17:28:43 +0300

    Wrapped all migrations in transactions for atomicity

--- server/src/migrations/20231026_00_initialise_users.ts
+++ server/src/migrations/20231026_00_initialise_users.ts
@@ -2,20 +2,28 @@ import { DataTypes } from 'sequelize';
 import { Migration } from '../utils/umzug';
 
 export const up: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().createTable('users', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
-    },
-    email: { type: DataTypes.STRING, unique: true, allowNull: false },
-    passwordHash: { type: DataTypes.STRING, allowNull: false },
-    createdAt: { type: DataTypes.DATE, allowNull: false },
-    updatedAt: { type: DataTypes.DATE, allowNull: false },
+  return sequelize.transaction(async (transaction) => {
+    await sequelize.getQueryInterface().createTable(
+      'users',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+          allowNull: false,
+        },
+        email: { type: DataTypes.STRING, unique: true, allowNull: false },
+        passwordHash: { type: DataTypes.STRING, allowNull: false },
+        createdAt: { type: DataTypes.DATE, allowNull: false },
+        updatedAt: { type: DataTypes.DATE, allowNull: false },
+      },
+      { transaction }
+    );
   });
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().dropTable('users');
+  return sequelize.transaction(async (transaction) => {
+    await sequelize.getQueryInterface().dropTable('users', { transaction });
+  });
 };
--- server/src/migrations/20231027_01_initialise_categories_and_products.ts
+++ server/src/migrations/20231027_01_initialise_categories_and_products.ts
@@ -2,65 +2,80 @@ import { DataTypes, Op } from 'sequelize';
 import { Migration } from '../utils/umzug';
 
 export const up: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().createTable('categories', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
-    },
-    name: { type: DataTypes.STRING, unique: true, allowNull: false },
-    created_at: { type: DataTypes.DATE, allowNull: false },
-    updated_at: { type: DataTypes.DATE, allowNull: false },
-  });
-
-  await sequelize.getQueryInterface().createTable('products', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'users',
-        key: 'id',
+  return sequelize.transaction(async (transaction) => {
+    await sequelize.getQueryInterface().createTable(
+      'categories',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+          allowNull: false,
+        },
+        name: { type: DataTypes.STRING, unique: true, allowNull: false },
+        created_at: { type: DataTypes.DATE, allowNull: false },
+        updated_at: { type: DataTypes.DATE, allowNull: false },
       },
-      onDelete: 'CASCADE',
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'categories',
-        key: 'id',
-      },
-    },
-    title: { type: DataTypes.STRING, allowNull: false },
-    price: { type: DataTypes.INTEGER, allowNull: false },
-    description: { type: DataTypes.STRING, allowNull: false },
-    image_urls: { type: DataTypes.STRING, allowNull: false },
-    status: {
-      type: DataTypes.ENUM('available', 'sold', 'pending'),
-      allowNull: false,
-      defaultValue: 'available',
-    },
-    created_at: { type: DataTypes.DATE, allowNull: false },
-    updated_at: { type: DataTypes.DATE, allowNull: false },
-  });
+      { transaction }
+    );
 
-  await sequelize.getQueryInterface().addConstraint('products', {
-    fields: ['price'],
-    type: 'check',
-    where: {
-      [Op.and]: [{ price: { [Op.gte]: 0 } }, { price: { [Op.lte]: 30000 } }],
-    },
-    name: 'check_price_range',
+    await sequelize.getQueryInterface().createTable(
+      'products',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+          allowNull: false,
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'users',
+            key: 'id',
+          },
+          onDelete: 'CASCADE',
+        },
+        category_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'categories',
+            key: 'id',
+          },
+        },
+        title: { type: DataTypes.STRING, allowNull: false },
+        price: { type: DataTypes.INTEGER, allowNull: false },
+        description: { type: DataTypes.STRING, allowNull: false },
+        image_urls: { type: DataTypes.STRING, allowNull: false },
+        status: {
+          type: DataTypes.ENUM('available', 'sold', 'pending'),
+          allowNull: false,
+          defaultValue: 'available',
+        },
+        created_at: { type: DataTypes.DATE, allowNull: false },
+        updated_at: { type: DataTypes.DATE, allowNull: false },
+      },
+      { transaction }
+    );
+
+    await sequelize.getQueryInterface().addConstraint('products', {
+      fields: ['price'],
+      type: 'check',
+      where: {
+        [Op.and]: [{ price: { [Op.gte]: 0 } }, { price: { [Op.lte]: 30000 } }],
+      },
+      name: 'check_price_range',
+      transaction,
+    });
   });
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().dropTable('products');
-  await sequelize.getQueryInterface().dropTable('categories');
+  return sequelize.transaction(async (transaction) => {
+    await sequelize.getQueryInterface().dropTable('products', { transaction });
+    await sequelize
+      .getQueryInterface()
+      .dropTable('categories', { transaction });
+  });
 };
