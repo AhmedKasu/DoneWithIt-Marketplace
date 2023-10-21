44c3d7ad2174ea613f7ad6367fdc18510986916a
Ahmed Kasu <ankassu@gmail.com>
2023-10-21 14:37:01 +0300

    added product & category initialisation migration

---
+++ server/src/migrations/20231027_01_initialise_categories_and_products.ts
@@ -0,0 +1,66 @@
+import { DataTypes, Op } from 'sequelize';
+import { Migration } from '../utils/umzug';
+
+export const up: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().createTable('categories', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+    createdAt: { type: DataTypes.DATE, allowNull: false },
+    updatedAt: { type: DataTypes.DATE, allowNull: false },
+  });
+
+  await sequelize.getQueryInterface().createTable('products', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'categories',
+        key: 'id',
+      },
+    },
+    title: { type: DataTypes.STRING, allowNull: false },
+    price: { type: DataTypes.INTEGER, allowNull: false },
+    description: { type: DataTypes.STRING, allowNull: false },
+    image_urls: { type: DataTypes.STRING, allowNull: false },
+    status: {
+      type: DataTypes.ENUM('available', 'sold', 'pending'),
+      allowNull: false,
+      defaultValue: 'available',
+    },
+    createdAt: { type: DataTypes.DATE, allowNull: false },
+    updatedAt: { type: DataTypes.DATE, allowNull: false },
+  });
+
+  await sequelize.getQueryInterface().addConstraint('Products', {
+    fields: ['price'],
+    type: 'check',
+    where: {
+      [Op.and]: [{ price: { [Op.gte]: 0 } }, { price: { [Op.lte]: 30000 } }],
+    },
+    name: 'check_price_range',
+  });
+};
+
+export const down: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().dropTable('products');
+  await sequelize.getQueryInterface().dropTable('categories');
+};
