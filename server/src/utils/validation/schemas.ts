2989f64c80a9741132a23459a54cc2a0ea7f1555
Ahmed Kasu <ankassu@gmail.com>
2023-10-22 13:09:44 +0300

    added a strong user password validation

--- server/src/utils/validation/schemas.ts
+++ server/src/utils/validation/schemas.ts
@@ -5,13 +5,14 @@ const userSchema = z.object({
     .string()
     .min(3, 'Name should have at least 3 characters.')
     .max(50, 'Name should not exceed 50 characters.'),
-
   email: z.string().email('Invalid email address.'),
-
   password: z
     .string()
-    .min(8, 'Password should have at least 8 characters.')
-    .max(30, 'Password should not exceed 30 characters.'),
+    .refine(
+      (password) =>
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(password),
+      'Password must be at least 8 characters long, contain at least one lowercase letter, one uppercase letter, one number, and one special character.'
+    ),
 });
 
 export { userSchema };
