d1712f650a7f57186f68dbb7ce92d40296201264
Ahmed Kasu <ankassu@gmail.com>
2023-10-22 15:03:21 +0300

    added cookie-parser to the app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import 'express-async-errors';
 import express from 'express';
 import 'reflect-metadata';
+import cookieParser from 'cookie-parser';
 
 import { PORT } from './utils/config';
 import { connectToDatabase } from './utils/db';
@@ -12,6 +13,7 @@ import usersRouter from './controllers/users';
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/users', usersRouter);
 
