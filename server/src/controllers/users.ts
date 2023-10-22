54ecdbaa08a83e3ea0b3ab0c17a313d1ef278f83
Ahmed Kasu <ankassu@gmail.com>
2023-10-22 16:15:40 +0300

    added a users/me route to return the current user

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,4 +1,4 @@
-import Router from 'express';
+import Router, { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import _ from 'lodash';
 
@@ -6,6 +6,8 @@ import { User } from '../models/User';
 
 import validateUserInput from '../utils/validation';
 import { userSchema } from '../utils/validation/schemas';
+
+import auth from '../middleware/auth';
 
 const router = Router();
 
@@ -24,6 +26,10 @@ router.post('/', async (req, res) => {
   res
     .status(200)
     .json(_.omit(newUser.toJSON(), ['passwordHash', 'createdAt', 'updatedAt']));
+});
+
+router.get('/me', auth, (req: Request, res: Response) => {
+  res.status(200).json(req.user);
 });
 
 export default router;
