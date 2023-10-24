0d5260d91b988106534005b2c8924fddad8b0f76
Ahmed Kasu <ankassu@gmail.com>
2023-10-24 18:42:12 +0300

    added a reusable findById middleware

---
+++ server/src/middleware/findById.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+import { ZodSchema } from 'zod';
+import { ModelStatic, Model, FindOptions } from 'sequelize/types';
+
+import validateUserInput from '../utils/validation/index';
+import { Product, Category } from '../types';
+import { EntityKeys, ReqUser } from '../types/express';
+
+const findById = <T extends Model>(
+  Model: ModelStatic<T>,
+  resource: EntityKeys,
+  validationSchema: ZodSchema<string>,
+  optionsFn?: (req: Request) => FindOptions
+) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const id = validateUserInput(validationSchema, req.params.id);
+    const options = optionsFn ? optionsFn(req) : undefined;
+
+    const modelInstance = await Model.findByPk(id, options);
+
+    req.entities = req.entities || {};
+    req.entities[resource] = modelInstance as
+      | Product
+      | Category
+      | ReqUser
+      | null;
+
+    if (!req.entities[resource])
+      return res.status(404).send(`${resource} not found!`);
+
+    return next();
+  };
+};
+
+export default findById;
