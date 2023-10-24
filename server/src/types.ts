50483cec623e30162566757bc02ff4dbd6869991
Ahmed Kasu <ankassu@gmail.com>
2023-10-24 17:35:47 +0300

    added product, user & category typescript interfaces

---
+++ server/src/types.ts
@@ -0,0 +1,29 @@
+export interface Product {
+  id: number;
+  userId: number;
+  categoryId: number;
+  title: string;
+  price: number;
+  description: string;
+  imageUrls: string[];
+  status: 'available' | 'sold' | 'pending';
+  condition: 'New' | 'Used - Like New' | 'Used - Good' | 'Used - Fair';
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  passwordHash: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
