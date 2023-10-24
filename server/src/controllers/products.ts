652245603ef1e66f339efba63d7adf9eaf00f31e
Ahmed Kasu <ankassu@gmail.com>
2023-10-24 19:09:40 +0300

    added routes for posting a new product and retrieve a single product

--- server/src/controllers/products.ts
+++ server/src/controllers/products.ts
@@ -1,7 +1,14 @@
 import Router, { Request, Response } from 'express';
+import _ from 'lodash';
 
 import { Product } from '../models/Product';
 import { Category } from '../models/Category';
+import { User } from '../models/User';
+
+import validateUserInput from '../utils/validation';
+import { productSchema, paramsIdSchema } from '../utils/validation/schemas';
+
+import findById from '../middleware/findById';
 
 const router = Router();
 
@@ -12,5 +19,29 @@ router.get('/', async (_req: Request, res: Response) => {
   });
   res.status(200).json(allProducts);
 });
+
+router.post('/', async (req: Request, res: Response) => {
+  const newProduct = await Product.create({
+    ...validateUserInput(productSchema, req.body),
+    userId: req.entities?.user?.id,
+  });
+  res.status(200).json(_.omit(newProduct.toJSON(), ['createdAt', 'updatedAt']));
+});
+
+router.get(
+  '/:id',
+  findById(Product, 'product', paramsIdSchema, (_req) => {
+    return {
+      attributes: { exclude: ['categoryId', 'userId'] },
+      include: [
+        { model: User, as: 'seller', attributes: ['id', 'name'] },
+        { model: Category, attributes: ['name'] },
+      ],
+    };
+  }),
+  (req: Request, res: Response) => {
+    res.status(200).json(req.entities?.product);
+  }
+);
 
 export default router;
