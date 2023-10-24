822ce983ef5ef41abf7cb2b55c5b5a4181399bcb
Ahmed Kasu <ankassu@gmail.com>
2023-10-24 18:22:51 +0300

    Enhanced Express request type definitions with structured entities

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,13 +1,19 @@
 import 'express';
+import type { Product, Category, User } from '../types';
+
+export type ReqUser = Omit<User, 'email' | 'passwordHash'>;
+
+export type EntityKeys = 'product' | 'category' | 'user';
+
+type Entities = {
+  [key in EntityKeys]?: Product | Category | ReqUser | null;
+};
 
 declare module 'express' {
   export interface Request {
     cookies?: {
       accessToken: string;
     };
-    user?: {
-      id: string;
-      name: string;
-    };
+    entities?: Entities;
   }
 }
