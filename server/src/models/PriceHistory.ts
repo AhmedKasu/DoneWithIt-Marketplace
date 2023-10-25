68764da0243883a565cc1cb850293557ff863314
Ahmed Kasu <ankassu@gmail.com>
2023-10-25 10:26:43 +0300

    added PriceHistory model

---
+++ server/src/models/PriceHistory.ts
@@ -0,0 +1,46 @@
+import {
+  Model,
+  Table,
+  Column,
+  PrimaryKey,
+  AutoIncrement,
+  ForeignKey,
+  DataType,
+  BelongsTo,
+} from 'sequelize-typescript';
+
+import { Product } from './Product';
+import { requiredIntegerColumn } from '../helpers/modelHelpers';
+
+@Table({ timestamps: true })
+export class PriceHistory extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(requiredIntegerColumn())
+  id!: number;
+
+  @ForeignKey(() => Product)
+  @Column(requiredIntegerColumn())
+  product_id!: number;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+    validate: {
+      max: {
+        args: [30000],
+        msg: 'Price cannot exceed 30,000',
+      },
+      min: {
+        args: [1],
+        msg: 'Price must be greater than 0',
+      },
+    },
+  })
+  price!: number;
+
+  @BelongsTo(() => Product, {
+    onDelete: 'CASCADE',
+  })
+  product!: Product;
+}
