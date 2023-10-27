a9016730b8fb42be1da57a0e1a2b8987a047ab45
Ahmed Kasu <ankassu@gmail.com>
2023-10-27 12:59:37 +0300

    added type Categories for all avalable category names

--- server/src/types.ts
+++ server/src/types.ts
@@ -20,9 +20,21 @@ export interface PriceHistory {
   updatedAt?: Date;
 }
 
+type Categories =
+  | 'Electronics'
+  | 'Entertainment'
+  | 'Free Stuff'
+  | 'Garden & Outdoor'
+  | 'Clothing'
+  | 'Home Appliances'
+  | 'Hobbies'
+  | 'Sporting Goods'
+  | 'Toys & Games'
+  | 'Pet Supplies';
+
 export interface Category {
   id: number;
-  name: string;
+  name: Categories;
   createdAt?: Date;
   updatedAt?: Date;
 }
