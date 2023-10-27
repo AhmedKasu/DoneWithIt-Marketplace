8e4dd2d020fd229fe778a78b83c77f287ec063eb
Ahmed Kasu <ankassu@gmail.com>
2023-10-27 12:22:41 +0300

    extended products route to fetch products based on a search query

--- server/src/services/products.ts
+++ server/src/services/products.ts
@@ -1,18 +1,34 @@
 import { Request } from 'express';
 import { Product } from '../types';
-import { WhereOptions } from 'sequelize';
+import { WhereOptions, Op } from 'sequelize';
 
 import { Category } from '../models/Category';
 
 import validateUserInput from '../utils/validation';
-import { productStatusSchema } from '../utils/validation/schemas';
+import {
+  productStatusSchema,
+  searchQuerySchema,
+} from '../utils/validation/schemas';
 
 export const productsQueryOptions = (req: Request) => {
   const statusQuery = req.query.status && req.query;
+  const searchQuery = req.query.search;
 
   const where: WhereOptions<Product> = {};
   if (statusQuery)
     where.status = validateUserInput(productStatusSchema, statusQuery).status;
+
+  if (searchQuery) {
+    const validatedSearchQuery = validateUserInput(
+      searchQuerySchema,
+      searchQuery
+    );
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    where[Op.or as any] = [
+      { title: { [Op.iLike]: `%${validatedSearchQuery}%` } },
+      { description: { [Op.iLike]: `%${validatedSearchQuery}%` } },
+    ];
+  }
 
   return {
     attributes: { exclude: ['categoryId'] },
