7342c50000f50898393b8f1a319647e6e959e042
Ahmed Kasu <ankassu@gmail.com>
2023-10-30 22:48:48 +0200

    client: Extended useSignup hook to navigate to the Signin page onSuccess

--- client/src/hooks/useSignUp.ts
+++ client/src/hooks/useSignUp.ts
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
+import { useNavigate } from 'react-router';
 
 import APIClient from '../services/apiClient';
 
@@ -15,8 +16,14 @@ type ResData = Omit<SignupData, 'password'>;
 const apiClient = new APIClient<SignupData>('/users');
 
 const useSignup = () => {
+  const navigate = useNavigate();
   return useMutation<ResData, AxiosError<ErrorResponse>, SignupData>({
     mutationFn: apiClient.post,
+    onSuccess: () => {
+      setTimeout(() => {
+        navigate('/signin');
+      }, 3000);
+    },
   });
 };
 
