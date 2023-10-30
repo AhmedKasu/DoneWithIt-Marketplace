db7806cdf99491d271ae3e11273ae060c6d86521
Ahmed Kasu <ankassu@gmail.com>
2023-10-30 11:54:27 +0200

    client: added a reusable react hook form

---
+++ client/src/components/forms/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+import { FieldValues, useFormContext } from 'react-hook-form';
+
+interface Props {
+  children: React.ReactNode;
+  // eslint-disable-next-line no-unused-vars
+  onSubmit: (variables: FieldValues) => Promise<void> | void;
+}
+
+export default function Form({ children, onSubmit }: Props) {
+  const { handleSubmit } = useFormContext();
+  return (
+    <Box
+      component='form'
+      noValidate
+      onSubmit={handleSubmit(onSubmit)}
+      sx={{ mt: 3 }}>
+      <Grid container spacing={2}>
+        {children}
+      </Grid>
+    </Box>
+  );
+}
