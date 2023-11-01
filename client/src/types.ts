e65d4cd72620e6ddfb1908a6bb3b5512fc3d5461
Ahmed Kasu <ankassu@gmail.com>
2023-11-01 15:20:38 +0200

    client: added Product interface

--- client/src/types.ts
+++ client/src/types.ts
@@ -5,3 +5,19 @@ export interface ErrorResponse {
 }
 
 export type CustomAxiosError = AxiosError<ErrorResponse>;
+
+export interface Product {
+  id: number;
+  userId: number;
+  categoryId: {
+    name: string;
+  };
+  title: string;
+  price: number;
+  description: string;
+  imageUrls: string[];
+  status: 'available' | 'sold' | 'pending';
+  condition: 'New' | 'Used - Like New' | 'Used - Good' | 'Used - Fair';
+  createdAt?: Date;
+  updatedAt?: Date;
+}
