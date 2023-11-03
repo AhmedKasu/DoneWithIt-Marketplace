638e90086cd2589a618c9fb4e05c3e1032e45ae7
Ahmed Kasu <ankassu@gmail.com>
2023-11-03 14:38:06 +0200

    client: extended mui PaletteOptions type

---
+++ client/types/mui.d.ts
@@ -0,0 +1,10 @@
+import '@mui/material/styles/createPalette';
+declare module '@mui/material/styles/createPalette' {
+  interface PaletteOptions {
+    appBg: {
+      main: string;
+      navbar: string;
+      sidebar: string;
+    };
+  }
+}
