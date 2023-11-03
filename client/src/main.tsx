f51bdc62079bcfdac9d04f0a1475c8c45913321c
Ahmed Kasu <ankassu@gmail.com>
2023-11-03 14:39:55 +0200

    client: added a custom theme & default app background colors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import theme from './themes/customTheme';
 
 import router from './Routing/Routes';
 
@@ -10,7 +14,10 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ client/src/themes/customTheme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    appBg: {
+      main: '#eceff1',
+      navbar: '#ffffff',
+      sidebar: '#ffffff',
+    },
+  },
+});
+
+export default theme;
