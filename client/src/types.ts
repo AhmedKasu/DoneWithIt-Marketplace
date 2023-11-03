537bf5f4b2ba82836d45e5af303852a18791a066
Ahmed Kasu <ankassu@gmail.com>
2023-11-03 10:06:34 +0200

    client: added type Categories

--- client/src/types.ts
+++ client/src/types.ts
@@ -21,3 +21,15 @@ export interface Product {
   createdAt?: Date;
   updatedAt?: Date;
 }
+
+export type Categories =
+  | 'Electronics'
+  | 'Entertainment'
+  | 'Free Stuff'
+  | 'Garden & Outdoor'
+  | 'Clothing'
+  | 'Home Appliances'
+  | 'Hobbies'
+  | 'Sporting Goods'
+  | 'Toys & Games'
+  | 'Pet Supplies';
