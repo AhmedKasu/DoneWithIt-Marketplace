b48a579d0f72b924a17efc8c5de05dbee23507f1
Ahmed Kasu <ankassu@gmail.com>
2023-11-06 12:29:07 +0200

    client: added a custom mui breakpoint

--- client/src/themes/customTheme.ts
+++ client/src/themes/customTheme.ts
@@ -1,9 +1,19 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+      xxl: 1895,
+    },
+  },
   palette: {
     appBg: {
-      main: '#eceff1',
+      main: '#F0F3F4',
       navbar: '#ffffff',
       sidebar: '#ffffff',
     },
--- client/types/mui.d.ts
+++ client/types/mui.d.ts
@@ -1,5 +1,13 @@
-import '@mui/material/styles/createPalette';
-declare module '@mui/material/styles/createPalette' {
+import '@mui/material';
+declare module '@mui/material/styles' {
+  interface BreakpointOverrides {
+    xs: true;
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
+    xxl: true;
+  }
   interface PaletteOptions {
     appBg: {
       main: string;
