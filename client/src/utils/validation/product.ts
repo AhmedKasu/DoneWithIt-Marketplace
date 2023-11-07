fac3fb8a59eb626bfb2f579e69ebf37318a67639
Ahmed Kasu <ankassu@gmail.com>
2023-11-07 16:30:25 +0200

    client: modified product searchQuerySchema  to accept an empty string

--- client/src/utils/validation/product.ts
+++ client/src/utils/validation/product.ts
@@ -3,7 +3,10 @@ const searchQuerySchema = z.object({
   search: z
     .string()
     .max(30, 'Search term is too long')
-    .regex(/^[a-zA-Z0-9\s\-_]+$/, 'Search term contains invalid characters')
+    .regex(
+      /^(?:[a-zA-Z0-9\s\-_]+)?$/,
+      'Search term contains invalid characters'
+    )
     .optional(),
 });
 
