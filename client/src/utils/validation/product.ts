2784d5d9d839495632c43b11d1eae62bcf3d2def
Ahmed Kasu <ankassu@gmail.com>
2023-11-10 16:27:29 +0200

    client: added new product validation schema

--- client/src/utils/validation/product.ts
+++ client/src/utils/validation/product.ts
@@ -10,4 +10,25 @@ const searchQuerySchema = z.object({
     .optional(),
 });
 
-export { searchQuerySchema };
+const productSchema = z.object({
+  title: z
+    .string()
+    .min(3, 'Title should have at least 3 characters.')
+    .max(50, 'Title should not exceed 50 characters.'),
+  price: z
+    .number()
+    .min(1, 'Price must be greater than 0')
+    .max(30000, 'Price cannot exceed 30,000'),
+  categoryId: z.number().int().positive(),
+  condition: z.enum(['New', 'Used - Like New', 'Used - Good', 'Used - Fair']),
+  description: z
+    .string()
+    .min(10, 'Description is too short! Should be at least 10 characters.')
+    .max(200, 'Description is too long! Should not exceed 200 characters.'),
+  imageUrls: z
+    .array(z.string().url('Invalid image URL.'))
+    .min(1, 'Please provide at least one image.')
+    .max(4, 'You can provide a maximum of 4 images.'),
+});
+
+export { searchQuerySchema, productSchema };
