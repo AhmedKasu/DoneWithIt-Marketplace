3eff565fd06221b3eaec9d3e3db4fdce10d09369
Ahmed Kasu <ankassu@gmail.com>
2023-11-17 12:11:15 +0200

    client: implemented fetching a single product

---
+++ client/src/hooks/useGetProduct.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { Product, CustomAxiosError } from '../types';
+
+import APIClient from '../services/apiClient';
+
+const apiClient = new APIClient<ResData>('/products');
+
+interface ResData extends Product {
+  priceHistories: [{ price: number; createdAt: string }];
+  seller: {
+    id: number;
+    name: string;
+    createdAt: string;
+  };
+}
+
+const useGetProduct = (productId?: string) => {
+  return useQuery<ResData, CustomAxiosError>({
+    queryKey: ['products', productId],
+    queryFn: () => apiClient.getOne(productId as string),
+    staleTime: 60 * 60 * 1000, // 1 hour
+  });
+};
+
+export default useGetProduct;
