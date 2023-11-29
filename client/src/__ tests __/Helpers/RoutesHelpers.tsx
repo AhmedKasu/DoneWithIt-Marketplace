8408bc32af8b2a612d594e9321e22e2f84610d34
Ahmed Kasu <ankassu@gmail.com>
2023-11-29 14:13:37 +0200

    client: added a routes tests helper function

---
+++ client/src/__ tests __/Helpers/RoutesHelpers.tsx
@@ -0,0 +1,42 @@
+import { ReactElement, isValidElement } from 'react';
+import { render } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+
+import { AuthProvider } from '../../context/authContext';
+import { ScreenBreakpointsProvider } from '../../context/screenBreakpoints';
+import { FiltersProvider } from '../../context/FiltersContext';
+
+interface RenderOptions {
+  element: ReactElement;
+  path: string;
+}
+
+const isReactElement = (obj: unknown): obj is ReactElement => {
+  return typeof obj === 'object' && obj !== null && isValidElement(obj);
+};
+
+const renderWithRouter = (
+  children: ReactElement | RenderOptions,
+  routes: RenderOptions[] = []
+): ReturnType<typeof render> => {
+  const options: RenderOptions = isReactElement(children)
+    ? { element: children, path: '/' }
+    : children;
+
+  const router = createMemoryRouter([{ ...options }, ...routes], {
+    initialEntries: [options.path],
+    initialIndex: 1,
+  });
+
+  return render(
+    <AuthProvider>
+      <ScreenBreakpointsProvider>
+        <FiltersProvider>
+          <RouterProvider router={router} />
+        </FiltersProvider>
+      </ScreenBreakpointsProvider>
+    </AuthProvider>
+  );
+};
+
+export default renderWithRouter;
