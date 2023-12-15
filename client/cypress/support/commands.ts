1507b3e91c761e88ddbda505157ac77e490f39d8
Ahmed Kasu <ankassu@gmail.com>
2023-12-15 15:53:10 +0200

    client: added a cypress custom login command & its custom type

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,2 +1,9 @@
 /// <reference types="cypress" />
 import 'cypress-file-upload';
+
+Cypress.Commands.add('login', (credentials) => {
+  cy.visit('http://localhost:5173/signin');
+  cy.get('#email').type(credentials.email);
+  cy.get('#password').type(credentials.password);
+  cy.get('button').contains('Sign In').click();
+});
