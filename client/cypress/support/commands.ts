b5c15c68aa52640d9cbedb6b3c14811146770fb7
Ahmed Kasu <ankassu@gmail.com>
2023-12-16 12:01:38 +0200

    client: handled cypress ResizeObserver error

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,10 +1,15 @@
 /// <reference types="cypress" />
 import 'cypress-file-upload';
 
+Cypress.on(
+  'uncaught:exception',
+  (err) => !err.message.includes('ResizeObserver loop limit exceeded')
+);
+
 Cypress.Commands.add('login', (credentials) => {
   cy.visit('http://localhost:5173/signin');
   cy.wait(2000);
-  cy.get('#email').type(credentials.email);
-  cy.get('#password').type(credentials.password);
+  cy.get('#email').should('not.be.disabled').type(credentials.email);
+  cy.get('#password').should('not.be.disabled').type(credentials.password);
   cy.get('button').contains('Sign In').click();
 });
