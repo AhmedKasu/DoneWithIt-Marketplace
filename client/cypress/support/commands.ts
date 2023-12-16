c3bf557a3b7bafd8ea6aae056f074e2652acb0a7
Ahmed Kasu <ankassu@gmail.com>
2023-12-16 10:51:58 +0200

    client: added 2 second wait time in the cypress custom login command

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -3,6 +3,7 @@ import 'cypress-file-upload';
 
 Cypress.Commands.add('login', (credentials) => {
   cy.visit('http://localhost:5173/signin');
+  cy.wait(2000);
   cy.get('#email').type(credentials.email);
   cy.get('#password').type(credentials.password);
   cy.get('button').contains('Sign In').click();
