7b7c95767142556f929bed231de34233a6cac128
Ahmed Kasu <ankassu@gmail.com>
2023-12-18 20:01:54 +0200

    client: added cypress e2e specPattern to control the order of the tests execution

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    // other configuration options...
+    specPattern: [
+      'cypress/e2e/spec.home-page.cy.ts',
+      'cypress/e2e/spec.createListing-page.cy.ts',
+      'cypress/e2e/spec.userListings-page.cy.ts',
+    ],
   },
 });
