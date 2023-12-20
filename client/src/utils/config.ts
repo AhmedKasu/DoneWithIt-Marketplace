2864387ebe208be7c6faab567bb613e6c4b265d3
Ahmed Kasu <ankassu@gmail.com>
2023-12-20 14:50:27 +0200

    client: enforced required environment variables

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -8,4 +8,13 @@ const {
   VITE_CLOUDINARY_UPLOAD_URL: CLOUDINARY_UPLOAD_URL,
 } = import.meta.env as unknown as EnvVariables;
 
+function enforceEnvVariable(name: string): void {
+  if (!import.meta.env[name]) {
+    throw new Error(`Missing environment variable ${name}`);
+  }
+}
+
+enforceEnvVariable('VITE_CLOUDINARY_UPLOAD_PRESET');
+enforceEnvVariable('VITE_CLOUDINARY_UPLOAD_URL');
+
 export { CLOUDINARY_UPLOAD_PRESET, CLOUDINARY_UPLOAD_URL };
