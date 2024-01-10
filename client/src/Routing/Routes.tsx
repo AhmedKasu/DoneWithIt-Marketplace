72c2332fb0e6216007a93b9eeeefd365cd1ab003
Ahmed Kasu <ankassu@gmail.com>
2024-01-10 15:34:29 +0200

    client: added EditListing page to routes

--- client/src/Routing/Routes.tsx
+++ client/src/Routing/Routes.tsx
@@ -7,6 +7,7 @@ import Signup from '../pages/Signup';
 import Signin from '../pages/Signin';
 import Home from '../pages/Home';
 import CreateListing from '../pages/CreateListing';
+import EditListing from '../pages/EditListing';
 import Product from '../pages/Product';
 import UserListings from '../pages/UserListings';
 import ErrorPage from '../pages/ErrorPage';
@@ -27,6 +28,7 @@ const router = createBrowserRouter([
     children: [
       { path: 'createListing', element: <CreateListing /> },
       { path: 'products/:id', element: <Product /> },
+      { path: 'products/edit/:id', element: <EditListing /> },
       { path: 'me/selling', element: <UserListings /> },
     ],
   },
