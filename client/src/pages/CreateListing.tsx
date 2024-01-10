41e7cb8974c8fd1146194da4cda28f4a93e020d2
Ahmed Kasu <ankassu@gmail.com>
2024-01-10 15:32:32 +0200

    client: added ProductForm to the CreateListing component

--- client/src/pages/CreateListing.tsx
+++ client/src/pages/CreateListing.tsx
@@ -1,185 +1,36 @@
-import { FieldValues, FormProvider, useForm } from 'react-hook-form';
+import { FieldValues } from 'react-hook-form';
 
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import InputAdornment from '@mui/material/InputAdornment';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import MenuItem from '@mui/material/MenuItem';
-
-import Form from '../components/Forms';
-import TextInput from '../components/Forms/TextInput';
-import ImagePicker from '../components/Forms/ImagePicker';
-
-import Error from '../components/Feedback/Error';
-import Success from '../components/Feedback/Success';
-import InteractiveCircularProgress from '../components/Feedback/InteractiveCircularProgress';
-
-import { productSchema } from '../utils/validation/product';
-import { productConditions } from '../utils/constants';
+import ProductForm from '../components/Product/ProductForm';
 
 import useGetCategories from '../hooks/useGetCategories';
 import useCreateListing from '../hooks/useCreateListing';
-
-export type FormData = z.infer<typeof productSchema>;
 
 export default function CreateListing() {
   const { data: categories } = useGetCategories();
   const {
     mutate: uploadImage,
-    data: newListing,
-    isSuccess: createProductSuccess,
-    isPending: createProductPending,
-    error: createProductError,
+    isSuccess,
+    isPending,
+    error,
   } = useCreateListing();
-
-  const methods = useForm<FormData>({
-    mode: 'onBlur',
-    defaultValues: {
-      categoryId: 1,
-      condition: productConditions[0],
-    },
-    resolver: zodResolver(productSchema),
-  });
-  const { formState } = methods;
 
   const handleSubmit = (variables: FieldValues) => {
     uploadImage(variables as FormData);
   };
 
-  const createProductErrorMessage = createProductError?.response?.data.details;
-
-  const renderFormContent = () => {
-    if (createProductPending)
-      return <InteractiveCircularProgress success={createProductSuccess} />;
-
-    if (newListing)
-      return (
-        <Success message='Listing successfully created' open={!!newListing} />
-      );
-
-    return (
-      <FormProvider {...methods}>
-        <Form onSubmit={handleSubmit}>
-          <ImagePicker name='imageUrls' />
-          <TextInput
-            name='title'
-            label='Title'
-            otherProps={{
-              id: 'title',
-              autoComplete: 'title',
-              required: true,
-              fullWidth: true,
-            }}
-          />
-          <TextInput
-            name='price'
-            label='Price'
-            type='number'
-            otherProps={{
-              id: 'price',
-              autoComplete: 'price',
-              required: true,
-              fullWidth: true,
-              InputProps: {
-                startAdornment: formState.dirtyFields.price && (
-                  <InputAdornment position='start'>€</InputAdornment>
-                ),
-              },
-            }}
-          />
-          {categories && categories.length > 0 && (
-            <TextInput
-              name='categoryId'
-              label='Category'
-              otherProps={{
-                id: 'category',
-                autoComplete: 'category',
-                required: true,
-                fullWidth: true,
-                select: true,
-                defaultValue: categories[0].id,
-              }}>
-              {categories.map((category) => (
-                <MenuItem key={category.id} value={category.id}>
-                  {category.name}
-                </MenuItem>
-              ))}
-            </TextInput>
-          )}
-          <TextInput
-            name='condition'
-            label='Condition'
-            otherProps={{
-              id: 'condition',
-              autoComplete: 'condition',
-              required: true,
-              fullWidth: true,
-              select: true,
-              defaultValue: productConditions[0],
-            }}>
-            {productConditions.map((condition, index) => (
-              <MenuItem key={index} value={condition}>
-                {condition}
-              </MenuItem>
-            ))}
-          </TextInput>
-          <TextInput
-            name='description'
-            label='Description'
-            otherProps={{
-              id: 'description',
-              autoComplete: 'description',
-              required: true,
-              fullWidth: true,
-              multiline: true,
-              rows: 4,
-            }}
-          />
-          <Grid item xs={12}>
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 2, mb: 2 }}>
-              Create Listing
-            </Button>
-          </Grid>
-        </Form>
-      </FormProvider>
-    );
-  };
+  const errorMessage = error?.response?.data.details;
 
   return (
-    <Box sx={{ height: '100vh', overflowY: 'scroll' }}>
-      <Container component='main' maxWidth='xs' sx={{ pt: { xs: 8, sm: 15 } }}>
-        <Box
-          sx={{
-            marginTop: { xs: 0, sm: 8 },
-            pb: { xs: 10 },
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-          <Error
-            message={createProductErrorMessage as string}
-            open={!!createProductError}
-          />
-
-          <Typography
-            component='h1'
-            variant='h5'
-            sx={{ mb: 3, fontWeight: 800 }}>
-            Item For Sale
-          </Typography>
-
-          {renderFormContent()}
-        </Box>
-      </Container>
-    </Box>
+    <>
+      {categories && (
+        <ProductForm
+          categories={categories}
+          handleSubmit={handleSubmit}
+          isPending={isPending}
+          isSuccess={isSuccess}
+          errorMessage={errorMessage}
+        />
+      )}
+    </>
   );
 }
