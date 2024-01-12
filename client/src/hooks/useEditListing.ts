b258669405b37a4b8a3871f46a7b6d45a3e8cdc1
Ahmed Kasu <ankassu@gmail.com>
2024-01-12 10:28:55 +0200

    client: modified useEditListing hook to properly handle a listing's images changes

--- client/src/hooks/useEditListing.ts
+++ client/src/hooks/useEditListing.ts
@@ -2,12 +2,14 @@ import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router';
 
 import APIClient from '../services/apiClient';
-import { FormData as UpdatedProduct } from '../components/Product/ProductForm';
+import useUploadImages from './useUploadImages';
 
+import { FormData as UpdatedProduct } from '../components/Product/ProductForm';
 import { CustomAxiosError } from '../types';
 
 interface Params {
   productId: number;
+  initialImageUrls: string[];
   updatedProduct: UpdatedProduct;
 }
 
@@ -20,9 +22,33 @@ const apiClient = new APIClient<UpdatedProduct, ResData>('/products');
 
 const useEditListing = () => {
   const navigate = useNavigate();
+  const uploadImages = useUploadImages();
   return useMutation<ResData, CustomAxiosError, Params>({
-    mutationFn: ({ productId, updatedProduct }) =>
-      apiClient.put(`/${productId}`, updatedProduct),
+    mutationFn: async ({ productId, initialImageUrls, updatedProduct }) => {
+      const newImageUrls = updatedProduct.imageUrls.filter(
+        (url) => !initialImageUrls.includes(url)
+      );
+
+      const deletedImageUrls = initialImageUrls.filter(
+        (url) => !updatedProduct.imageUrls.includes(url)
+      );
+
+      let finalImageUrls: string[] = [...initialImageUrls];
+
+      let uploadedImages: string[] = [];
+      if (newImageUrls.length > 0) {
+        uploadedImages = await uploadImages(newImageUrls);
+      }
+
+      finalImageUrls = finalImageUrls
+        .filter((url) => !deletedImageUrls.includes(url))
+        .concat(uploadedImages);
+
+      return apiClient.put(`/${productId}`, {
+        ...updatedProduct,
+        imageUrls: finalImageUrls,
+      });
+    },
     onSuccess: () => {
       setTimeout(() => {
         navigate('/');
