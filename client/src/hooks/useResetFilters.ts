9a33c2198e0c1c47b98b9195460803aa7ef39d32
Ahmed Kasu <ankassu@gmail.com>
2024-01-13 16:20:15 +0200

    client: added useResetFilters hook to reset filter values

---
+++ client/src/hooks/useResetFilters.ts
@@ -0,0 +1,17 @@
+import { useFiltersContext } from '../context/FiltersContext';
+
+const useResetFilters = () => {
+  const { setCondition, setMaxPrice, setMinPrice, setSearchQuery, setStatus } =
+    useFiltersContext();
+
+  const resetFilters = () => {
+    setCondition(undefined);
+    setMaxPrice(undefined);
+    setMinPrice(undefined);
+    setSearchQuery('');
+    setStatus(undefined);
+  };
+  return resetFilters;
+};
+
+export default useResetFilters;
