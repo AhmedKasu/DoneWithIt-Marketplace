6a894bde53349a492cec8580ea70ef3b56ccc6b4
Ahmed Kasu <ankassu@gmail.com>
2024-01-23 16:32:22 +0200

    client: moved the responsibility of the MessageIcon wrapper to its parrent component MessageBadge

--- client/src/components/NavBar/MessageBadge.tsx
+++ client/src/components/NavBar/MessageBadge.tsx
@@ -6,16 +6,35 @@ import MessageIcon from './MessageIcon';
 interface Props {
   unreadMessages?: number;
   onBadgeClick: () => void;
+  size?: number;
 }
 
 export default function MessageBadge({
   onBadgeClick,
   unreadMessages = 0,
+  size = 18,
 }: Props) {
+  const boxSize = size + 18;
   return (
     <Box onClick={onBadgeClick}>
       <Badge badgeContent={unreadMessages} color='primary'>
-        <MessageIcon />
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: boxSize,
+            backgroundColor: '#e9ebee',
+            height: boxSize,
+            borderRadius: boxSize / 2,
+            cursor: 'pointer',
+            '&:hover': {
+              backgroundColor: '#d8dadb',
+              cursor: 'pointer',
+            },
+          }}>
+          <MessageIcon size={size} />
+        </Box>
       </Badge>
     </Box>
   );
