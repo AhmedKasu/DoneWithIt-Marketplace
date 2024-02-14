1d385838fd17f977e8a270c9c9fd63155f463371
Ahmed Kasu <ankassu@gmail.com>
2024-02-14 14:30:52 +0200

    server: added chat functionality to socket server

--- server/src/utils/socket.ts
+++ server/src/utils/socket.ts
@@ -1,6 +1,16 @@
 import { Server } from 'socket.io';
 import { Server as HttpServer } from 'http';
 import { CLIENT_URL } from '../constants';
+import { Message as MessageType, InitChat } from '../types';
+
+import { ChatRoom } from '../models/ChatRoom';
+import { Message } from '../models/Message';
+
+interface ExistingChatEvent {
+  chatRoomId: string;
+  receiverId: number;
+  lastReadMessageId: number;
+}
 
 const setUpSocket = (server: HttpServer) => {
   const io = new Server(server, {
@@ -11,11 +21,72 @@ const setUpSocket = (server: HttpServer) => {
   });
 
   io.on('connection', (socket) => {
-    console.log('a user connected');
+    const userId = socket.handshake.query.userId;
+    const userRoom = `seller_${userId}`;
+    void socket.join(userRoom);
 
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+    socket.on(
+      'init_chat',
+      async ({ buyerId, sellerId, productId }: InitChat) => {
+        const chatRoomId = `${sellerId}-${buyerId}-${productId}`;
+
+        const existingChat = await ChatRoom.findOne({
+          where: { id: chatRoomId },
+        });
+
+        if (!existingChat) {
+          await ChatRoom.create({
+            id: chatRoomId,
+            buyerId,
+            sellerId,
+            productId,
+          });
+        }
+
+        const sellerRoom = `seller_${sellerId}`;
+        socket.to(sellerRoom).emit('new_chat', { chatRoomId });
+
+        void socket.join(chatRoomId);
+      }
+    );
+
+    socket.on(
+      'existing_chat',
+      async ({
+        chatRoomId,
+        receiverId,
+        lastReadMessageId,
+      }: ExistingChatEvent) => {
+        const chatRoom = await ChatRoom.findOne({ where: { id: chatRoomId } });
+        if (!chatRoom) return;
+
+        chatRoom.lastReadMessageId = lastReadMessageId;
+        await chatRoom.save();
+
+        const sellerRoom = `seller_${receiverId}`;
+        socket.to(sellerRoom).emit('new_chat', { chatRoomId });
+      }
+    );
+
+    socket.on('join_chat', ({ chatRoomId }: { chatRoomId: string }) => {
+      void socket.join(chatRoomId);
     });
+
+    socket.on(
+      'send_message',
+      async ({ content, chatRoomId: roomId, senderId }: MessageType) => {
+        await Message.create({
+          chatRoomId: roomId,
+          senderId,
+          content,
+        });
+        socket.to(roomId).emit('receive_message', {
+          chatRoomId: roomId,
+          senderId,
+          content,
+        });
+      }
+    );
   });
 };
 
