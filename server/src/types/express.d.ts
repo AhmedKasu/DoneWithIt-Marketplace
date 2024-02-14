83d72c50a6e6312d83a6a8d01857e4a08f8c875f
Ahmed Kasu <ankassu@gmail.com>
2024-02-14 14:19:22 +0200

    sever: added ChatRoomInstance to express.d.ts

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,17 +1,19 @@
 import 'express';
-import type { AuthUser, Product, Category, User } from '../types';
+import type { AuthUser, Product, Category, User, ChatRoom } from '../types';
 import { Model } from 'sequelize-typescript';
 
-export type EntityKeys = 'product' | 'category' | 'user';
+export type EntityKeys = 'product' | 'category' | 'user' | 'chatRoom';
 
 export interface ProductInstance extends Model<Product>, Product {}
 export interface CategoryInstance extends Model<Category>, Category {}
 export interface UserInstance extends Model<User>, ReqUser {}
+export interface ChatRoomInstance extends Model<ChatRoom>, ChatRoom {}
 
 export type EntitiyTypes =
   | ProductInstance
   | CategoryInstance
   | UserInstance
+  | ChatRoomInstance
   | null;
 
 export type Entities = {
