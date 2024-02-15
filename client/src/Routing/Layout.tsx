e4b19fbb24578fea8872246178667c63b631d5a0
Ahmed Kasu <ankassu@gmail.com>
2024-02-15 12:51:38 +0200

    client: added ChatRoom component to Layout

--- client/src/Routing/Layout.tsx
+++ client/src/Routing/Layout.tsx
@@ -2,15 +2,25 @@ import { Outlet } from 'react-router';
 import Box from '@mui/material/Box';
 
 import NavBar from '../components/NavBar/NavBar';
+import ChatRoom from '../components/ChatRoom';
+
+import { useChatRoomContext } from '../context/ChatRoomContext';
+import { useAuthContext } from '../context/authContext';
 
 import useRefetchCurrentUser from '../hooks/user/useRefetchCurrentUser';
 
 export default function Layout() {
   useRefetchCurrentUser();
+  const { openChatRooms } = useChatRoomContext();
+  const { currentUser } = useAuthContext();
 
   return (
     <>
       <NavBar />
+      {currentUser &&
+        openChatRooms.map((chatRoomId) => (
+          <ChatRoom key={chatRoomId} chatRoomId={chatRoomId} />
+        ))}
       <Box
         sx={{
           mt: 2,
