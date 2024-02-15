d26c72cbfa02d6ab21a98533ac2f769ab37f92bf
Ahmed Kasu <ankassu@gmail.com>
2024-02-15 15:57:23 +0200

    client: added unreadMessages persistence functionality to MessageBadge component

--- client/src/components/NavBar/MessageBadge.tsx
+++ client/src/components/NavBar/MessageBadge.tsx
@@ -1,12 +1,17 @@
+import { useEffect } from 'react';
 import Badge from '@mui/material/Badge';
 import Box from '@mui/material/Box';
 
 import MessageIcon from './MessageIcon';
 
+import useGetChatRooms from '../../hooks/chatRoom/useGetChatRooms';
+
 interface Props {
   unreadMessages: number;
+  setUnreadMessages: React.Dispatch<React.SetStateAction<string[]>>;
   onBadgeClick: () => void;
   isBadgeClicked: boolean;
+  currentUserId: number;
   size?: number;
 }
 
@@ -14,11 +19,39 @@ export default function MessageBadge({
   onBadgeClick,
   isBadgeClicked,
   unreadMessages,
+  setUnreadMessages,
+  currentUserId,
   size = 20,
 }: Props) {
+  const { data: chatRooms } = useGetChatRooms(currentUserId);
+
+  useEffect(() => {
+    if (!chatRooms) return;
+
+    const dbUnreadMessages = chatRooms.reduce<string[]>((acc, room) => {
+      if (room.messages.length === 0) return acc;
+
+      const lastMessage = room.messages[0];
+
+      if (lastMessage.senderId === currentUserId) return acc;
+
+      if (room.lastReadMessage.id !== room.messages[0].id) {
+        return [...acc, room.id];
+      }
+      return acc;
+    }, []);
+
+    if (dbUnreadMessages.length < 0) return;
+
+    setUnreadMessages(dbUnreadMessages);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chatRooms, currentUserId]);
+
   const boxSize = size + 16;
   const iconColor = isBadgeClicked ? '#1778F2' : 'black';
   const iconWrapperColor = isBadgeClicked ? '#dfe3ee' : '#e9ebee';
+
   return (
     <Box onClick={onBadgeClick}>
       <Badge badgeContent={unreadMessages} color='error'>
