65899a0136f3ea6a2d168d1316aa8dee0d65878b
Ahmed Kasu <ankassu@gmail.com>
2024-02-16 23:05:58 +0200

    server: added CORS_ORIGIN constant to constants

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,3 +1,9 @@
+import { NODE_ENV } from '../utils/config';
 const CLIENT_URL = 'http://localhost:5173';
 
-export { CLIENT_URL };
+const CORS_ORIGIN =
+  NODE_ENV === 'development' || NODE_ENV === 'test'
+    ? CLIENT_URL
+    : 'https://patientor.herokuapp.com';
+
+export { CLIENT_URL, CORS_ORIGIN };
