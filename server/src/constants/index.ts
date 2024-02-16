394a65daac6c5349fa67b99470f26f50190c1c53
Ahmed Kasu <ankassu@gmail.com>
2024-02-17 00:24:47 +0200

    server: fixed cors origin production url

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -4,6 +4,6 @@ const CLIENT_URL = 'http://localhost:5173';
 const CORS_ORIGIN =
   NODE_ENV === 'development' || NODE_ENV === 'test'
     ? CLIENT_URL
-    : 'https://patientor.herokuapp.com';
+    : 'https://donewithit-marketplace-6ab68864c928.herokuapp.com/';
 
 export { CLIENT_URL, CORS_ORIGIN };
