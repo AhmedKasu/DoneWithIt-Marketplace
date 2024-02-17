520191ea7c5a70bac6a3d6b85e764c82a0a49c18
Ahmed Kasu <ankassu@gmail.com>
2024-02-17 12:53:51 +0200

    client: implemented resetting openChatRooms state on signing out

--- client/src/hooks/auth/useSignOut.ts
+++ client/src/hooks/auth/useSignOut.ts
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router';
 
 import APIClient from '../../services/apiClient';
 import { useAuthContext } from '../../context/authContext';
+import { useChatRoomContext } from '../../context/ChatRoomContext';
 
 import { CustomAxiosError } from '../../types';
 
@@ -11,12 +12,14 @@ const apiClient = new APIClient<void>('/logout');
 const useSignout = () => {
   const navigate = useNavigate();
   const { setCurrentUser } = useAuthContext();
+  const { setOpenChatRooms } = useChatRoomContext();
   return useMutation<void, CustomAxiosError>({
     mutationFn: apiClient.post,
     onSuccess: () => {
       setTimeout(() => {
         navigate('/');
         setCurrentUser(undefined);
+        setOpenChatRooms([]);
       }, 300);
     },
   });
