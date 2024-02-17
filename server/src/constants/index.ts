c37593b9bb0f78a7d7b8c34af103813f662ad95b
Ahmed Kasu <ankassu@gmail.com>
2024-02-17 10:00:25 +0200

    server: updated CORS_ORIGIN constant to be CLIENT_URL only when not in production

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,9 +1,6 @@
 import { NODE_ENV } from '../utils/config';
-const CLIENT_URL = 'http://localhost:5173';
 
-const CORS_ORIGIN =
-  NODE_ENV === 'development' || NODE_ENV === 'test'
-    ? CLIENT_URL
-    : 'https://donewithit-marketplace-6ab68864c928.herokuapp.com/';
+const CLIENT_URL = 'http://localhost:5173';
+const CORS_ORIGIN = NODE_ENV === 'production' ? undefined : CLIENT_URL;
 
 export { CLIENT_URL, CORS_ORIGIN };
